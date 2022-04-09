cad572c1e5b739a238f08a8fc41c6133fa2f18ba
EELMMAD <elmiramadadi@gmail.com>
2022-04-09 22:41:23 +0200

    Created WhileAndDoWhileStatements.java

---
+++ src/main/java/se/udemy/elmira/WhileAndDoWhileStatements.java
@@ -0,0 +1,17 @@
+package se.udemy.elmira;
+
+public class WhileAndDoWhileStatements {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count != 6) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+
+        for (int i = 1; i<7; i++){
+            System.out.println("Count value is " + count);
+        }
+
+    }
+
+}
